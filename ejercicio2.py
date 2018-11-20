70826073c0680449b0bd71614a9efe228debf902
juanpablo1234 <44232012+juanpablo1234@users.noreply.github.com>
2018-11-20 08:42:59 -0500

    finalejercicio2
    ss

--- ejercicio2.py
+++ ejercicio2.py
@@ -10,11 +10,11 @@ n5 = int(input("Ingrese numero 5 = "))
 def menor_en_arreglo(n1,n2,n3,n4,n5):
     if n1 < n2 and n1 < n3 and n1 < n4 and n1 < n5:
         print(n1)
-    #elif n2 < n1 and n2 < n3 and n2 < n4 and n2 < n5:
-    #    print(n2)
-    #elif n3 < n1 and n3 < n2 and n3 < n4 and n3 < n5:
-    #    print(n3)
-    #elif n4 < n1 and n4 < n2 and n4 < n3 and n4 < n5:
-    #    print(n4)
-    #elif n5 < n1 and n5 < n2 and n5 < n3 and n5 < n4:
-    #    print(n5)
+    elif n2 < n1 and n2 < n3 and n2 < n4 and n2 < n5:
+        print(n2)
+    elif n3 < n1 and n3 < n2 and n3 < n4 and n3 < n5:
+        print(n3)
+    elif n4 < n1 and n4 < n2 and n4 < n3 and n4 < n5:
+        print(n4)
+    elif n5 < n1 and n5 < n2 and n5 < n3 and n5 < n4:
+        print(n5)
