201adaccd947f0d7a29bad044a00993919bf2b09
alumno informatica <alumno@ciis.usil.edu.pe>
2018-11-20 08:33:12 -0500

    Ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,20 @@
+# Inicio
+n1 = int(input("Ingrese numero 1 = "))
+n2 = int(input("Ingrese numero 2 = "))
+n3 = int(input("Ingrese numero 3 = "))
+n4 = int(input("Ingrese numero 4 = "))
+n5 = int(input("Ingrese numero 5 = "))
+
+# Proceso
+
+def menor_en_arreglo(n1,n2,n3,n4,n5):
+    if n1 < n2 and n1 < n3 and n1 < n4 and n1 < n5:
+        print(n1)
+    #elif n2 < n1 and n2 < n3 and n2 < n4 and n2 < n5:
+    #    print(n2)
+    #elif n3 < n1 and n3 < n2 and n3 < n4 and n3 < n5:
+    #    print(n3)
+    #elif n4 < n1 and n4 < n2 and n4 < n3 and n4 < n5:
+    #    print(n4)
+    #elif n5 < n1 and n5 < n2 and n5 < n3 and n5 < n4:
+    #    print(n5)
